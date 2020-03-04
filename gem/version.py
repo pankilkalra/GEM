974e331caac45f0e510e9938832aca3cd6e1fc6f
pankilkalra <pankil18061@iiitd.ac.in>
2020-03-04 19:25:09 +0530

    yoyoyo

---
+++ gem/version.py
@@ -0,0 +1,11 @@
+
+    # THIS FILE IS GENERATED FROM GEM SETUP.PY
+    short_version = '1.0.0'
+    version = '1.0.0'
+    full_version = '1.0.0'
+    git_revision = '82ba349e18bb700a9380db2827ad3beb45ff1731'
+    release = 'True'
+    if not release:
+        version = full_version
+        short_version += ".dev"
+    
